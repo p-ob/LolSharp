ef7a4947d219b7564628d9e0abc38328c7fec418
Patrick O'Brien <patrick.obrien91@gmail.com>
2016-05-15 16:13:22 -0500

    feat: templating out unit tests

---
+++ test/LolSharp.Tests/RiotClientSummonerTests.cs
@@ -0,0 +1,62 @@
+﻿namespace LolSharp.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using Moq;
+    using Xunit;
+    using RestSharp;
+    using RiotObjects.Summoner;
+
+    public class RiotClientSummonerTests
+    {
+        private const long SummonerId = 25886496;
+        private readonly SummonerDto _mockSummoner = new SummonerDto
+        {
+            Id = SummonerId,
+            Name = "Drunk7Irishman",
+            SummonerLevel = 30
+        };
+        private readonly Mock<IRiotClient> _riotClient;
+
+        public RiotClientSummonerTests()
+        {
+            //_restClient.Setup(c => c.ExecuteAsync(
+            //    It.IsAny<IRestRequest>(),
+            //    It.IsAny<Action<IRestResponse<Dictionary<string, SummonerDto>>, RestRequestAsyncHandle>>()))
+            //    .Callback<IRestRequest, Action<IRestResponse<Dictionary<string, SummonerDto>>, RestRequestAsyncHandle>>(
+            //        (request, callback) =>
+            //        {
+            //            var responseMock = new Mock<IRestResponse<Dictionary<string, SummonerDto>>>();
+            //            responseMock.Setup(r => r.Data).Returns(new Dictionary<string, SummonerDto> { { "drunk7irishman", _mockSummoner }});
+            //            callback(responseMock.Object, null);
+            //        });
+            _riotClient = new Mock<IRiotClient>();
+
+            _riotClient.Setup(c => c.GetSummoners(It.IsAny<IEnumerable<long>>(), null))
+                .ReturnsAsync(new Dictionary<string, SummonerDto> {{"drunk7irishman", _mockSummoner}});
+        }
+
+        [Fact]
+        public void TestGetExistingSummoner()
+        {
+            try
+            {
+            }
+            catch (RiotApiException e) when (e.StatusCode == HttpStatusCode.NotFound)
+            {
+            }
+        }
+
+        [Fact]
+        public void TestGetNonexistentSummoner()
+        {
+            try
+            {
+            }
+            catch (RiotApiException e) when (e.StatusCode == HttpStatusCode.NotFound)
+            {
+            }
+        }
+    }
+}
